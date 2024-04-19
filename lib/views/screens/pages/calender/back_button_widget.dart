94fa50e5df6238d35b3ccf4a471cf122a825921d
kana <kf0830s@gmail.com>
2024-04-19 19:04:12 +0900

    戻るボタンの背景色設定

--- lib/views/screens/pages/calender/back_button_widget.dart
+++ lib/views/screens/pages/calender/back_button_widget.dart
@@ -7,30 +7,35 @@ import '../../../../view_models/training_memo_notifier/calender_notifier.dart';
 import '../../../../view_models/training_memo_notifier/calender_part_notifier.dart';
 
 class BackButtonWidget extends ConsumerWidget {
-
   DateTime selectedDay;
   AsyncValue<List<QueryDocumentSnapshot<Map<String, dynamic>>>> menus;
   AsyncValue<Map<String, dynamic>> trainingPart;
 
-  BackButtonWidget({super.key, required this.selectedDay, required this.menus, required this.trainingPart});
-
+  BackButtonWidget(
+      {super.key,
+      required this.selectedDay,
+      required this.menus,
+      required this.trainingPart});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     return Container(
-      color: const Color(0xFF7b755e),
+      width: 100,
+      height: 100,
+      decoration: const BoxDecoration(color: Color(0xFF7b755e)),
       child: ElevatedButton(
-        child: Center(
-          child: Container(
-            color: const Color(0xFF7b755e),
-              child: const Icon(Icons.arrow_back,)),
+        style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all(const Color(0xFF7b755e))),
+        child: const Center(
+          child: Icon(
+            Icons.arrow_back,
+          ),
         ),
         onPressed: () {
           final notifier = ref.read(calenderNotifierProvider.notifier);
           notifier.setState(selectedDay);
-          final partNotifier =
-          ref.read(calenderPartNotifierProvider.notifier);
+          final partNotifier = ref.read(calenderPartNotifierProvider.notifier);
           Navigator.pop(context);
         },
       ),
